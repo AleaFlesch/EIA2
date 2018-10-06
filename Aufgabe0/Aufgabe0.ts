024cc663a1ae6a79d01626b890a88df5195eeaee
Alea Flesch <alea.flesch@sennrich-schneider.de>
2018-10-06 17:09:26 +0200

    Begrüßungsseite

---
+++ Aufgabe0/Aufgabe0.ts
@@ -0,0 +1,8 @@
+
+// Begrüßung
+
+<script language = "typescript">
+var name = "";
+name = prompt ("Wie heißt du?", "");
+document.write ("Hallo" + name + "schön dass du vorbei schaust" + "!");
+</script>
