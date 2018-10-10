5a0695f6321af2bd6c2ca4367fbdfea18eda2088
Alea Flesch <alea.flesch@sennrich-schneider.de>
2018-10-10 13:19:27 +0200

    html1 & js0 hochladen

--- Aufgabe0/Aufgabe0.js
+++ Aufgabe0/Aufgabe0.js
@@ -1,9 +1,9 @@
 // Begr��ung
 function Funktion() {
-    var name = prompt("Wie heisst du?");
+    var name = prompt("Wie hei�t du?");
     if (name != null) {
         document.getElementById("solution").innerHTML =
-            "Schoen dass du vorbei schaust " + name + " :)";
+            "Sch�n dass du vorbei schaust " + name + " :)";
     }
 }
 //# sourceMappingURL=Aufgabe0.js.map
