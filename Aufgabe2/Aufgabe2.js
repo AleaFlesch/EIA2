39feda1036b9de33d4d0ccedc929f5d3d9dabf68
Alea Flesch <alea.flesch@sennrich-schneider.de>
2018-10-26 16:06:12 +0200

    Aufgabe 2 erweitert

--- Aufgabe2/Aufgabe2.js
+++ Aufgabe2/Aufgabe2.js
@@ -1,27 +1,34 @@
 var Uno2;
 (function (Uno2) {
+    document.addEventListener("DOMContentLoaded", main);
     //Deklaration    
     let handcards;
     let colors = ["Red", "Blue", "Green", "Yellow"];
     let values = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+2", "x", "<->"];
     let deck;
+    for (let i = 0; i < colors.length; i++) {
+        for (let a = 0; a < values.length; a++) {
+            for (let z = 0; z < 2; z++) {
+                let card = { color: colors[i], value: values[a] };
+                deck.push(card);
+            }
+        }
+    }
     //promt function
     function main() {
         let x;
         let y = prompt("Wie viele Karten m�chtest du?");
         x = Number(y);
-        for (let i = 0; i < colors.length; i++) {
-            for (let a = 0; a < values.length; a++) {
-                for (let z = 0; z < 2; z++) {
-                    let card = { color: colors[i], value: values[a] };
-                    deck.push(card);
-                }
-            }
-        }
-        //rand    
-        function random(n) {
-            return Math.floor(Math.random() * Math.floor(n));
-        }
+        //Aufruf der Funktion        
+        random();
+    }
+    //random    
+    function random() {
+        let r = Math.floor(Math.random() * (deck.length - 1));
+        //Karte von deck in handcard kopiert       
+        handcards.push(deck[r]);
+        //kopierte Karte wurde aus deck gel�scht       
+        deck.splice(r);
     }
 })(Uno2 || (Uno2 = {}));
 //# sourceMappingURL=Aufgabe2.js.map
--- Aufgabe2/Aufgabe2.ts
+++ Aufgabe2/Aufgabe2.ts
@@ -1,5 +1,7 @@
 namespace Uno2 {
 
+    document.addEventListener("DOMContentLoaded", main);
+
     //interface
     interface Card {
         color: string;
@@ -12,26 +14,37 @@ namespace Uno2 {
     let values: string[] = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+2", "x", "<->"];
     let deck: Card[]
 
+    for (let i: number = 0; i < colors.length; i++) {
+        for (let a: number = 0; a < values.length; a++) {
+            for (let z: number = 0; z < 2; z++) {
+                let card: Card = { color: colors[i], value: values[a] };
+                deck.push(card);
+            }
+        }
+    }
+
     //promt function
-    function main() {
+    function main(): void {
         let x: number;
         let y: string = prompt("Wie viele Karten möchtest du?");
         x = Number(y);
 
-        for (let i: number = 0; i < colors.length; i++) {
-            for (let a: number = 0; a < values.length; a++) {
-                for (let z: number = 0; z < 2; z++) {
-                    let card: Card = {color : colors[i], value : values[a]};
-                    deck.push(card);
-                }
-            }
-        }
-
-        //rand    
-        function random(n: number) {
-            return Math.floor(Math.random() * Math.floor(n));
-        }
-
-
+        //Aufruf der Funktion        
+        random();
     }
+
+
+
+    //random    
+    function random() {
+        let r: number = Math.floor(Math.random() * (deck.length - 1));
+        
+        //Karte von deck in handcard kopiert       
+        handcards.push(deck[r]);
+
+        //kopierte Karte wurde aus deck gelöscht       
+        deck.splice(r);
+    }
+
+
 }
