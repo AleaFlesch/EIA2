d8fe8b570c8f267b7ef1dcedb9892e96dcfa8a2d
Alea Flesch <alea.flesch@sennrich-schneider.de>
2018-10-28 14:00:29 +0100

    Aufgabe2 weiter ausgearbeitet

--- Aufgabe2/Aufgabe2.js
+++ Aufgabe2/Aufgabe2.js
@@ -1,3 +1,13 @@
+/*
+Aufgabe: Aufgabe 2
+Name: Alea Flesch
+Matrikel: 259109
+Datum: 26.10.2018
+Hiermit versichere ich, dass ich diesen
+Code selbst in Zusammenarbeit mit Theresa Gut &
+mit Unterst�tzung von Lukas und Anna geschrieben habe. Er wurde
+nicht kopiert und auch nicht diktiert.
+*/
 var Uno2;
 (function (Uno2) {
     document.addEventListener("DOMContentLoaded", main);
@@ -15,8 +25,8 @@ var Uno2;
         }
     }
     //promt function
+    let x;
     function main() {
-        let x;
         let y = prompt("Wie viele Karten m�chtest du?");
         x = Number(y);
         //Aufruf der Funktion        
@@ -24,11 +34,65 @@ var Uno2;
     }
     //random    
     function random() {
-        let r = Math.floor(Math.random() * (deck.length - 1));
-        //Karte von deck in handcard kopiert       
-        handcards.push(deck[r]);
-        //kopierte Karte wurde aus deck gel�scht       
-        deck.splice(r);
+        for (let k = 0; k < x; k++) {
+            let r = Math.floor(Math.random() * (deck.length - 1));
+            handcards.push(deck[r]); //Karte von deck in handcard kopiert       
+            deck.splice(r); //kopierte Karte wurde aus deck gel�scht 
+        }
     }
+    // Inhalte f�r HTML
+    function placeDiv(_color, _v, _y) {
+        let div = document.createElement("div");
+        document.body.appendChild(div);
+        div.setAttribute("id", "a" + _y);
+        document.getElementById("a" + _y).innerHTML += _v;
+        let s = div.style;
+        s.border = "thin solid black";
+        s.textAlign = "center";
+        s.position = "absolute";
+        s.backgroundColor = _color;
+        s.width = 100 + "px";
+        s.height = 200 + "px";
+        s.left = (_y + 0.2) * 110 + "px";
+        s.bottom = 40 + "px";
+        if (_color == "black") {
+            s.color = "white";
+        }
+        if (_color == "blue") {
+            s.color = "white";
+        } //Schrift bei schwarzen und blauen Karten in wei� ge�ndert
+    }
+    function Stapel() {
+        let div = document.createElement("div");
+        document.body.appendChild(div);
+        div.setAttribute("id", "CardDeck");
+        document.getElementById("CardDeck").innerHTML += "CardDeck";
+        let s = div.style;
+        s.border = "solid black";
+        s.textAlign = "center";
+        s.position = "absolute";
+        s.backgroundColor = "lightgray";
+        s.width = 100 + "px";
+        s.height = 200 + "px";
+        s.left = 50 + "px";
+        s.top = 20 + "px";
+    }
+    function DiscardDeck() {
+        let div = document.createElement("div");
+        document.body.appendChild(div);
+        div.setAttribute("id", "DiscardDeck");
+        document.getElementById("DiscardDeck").innerHTML += "DiscardDeck";
+        let s = div.style;
+        s.border = "solid black";
+        s.textAlign = "center";
+        s.position = "absolute";
+        s.backgroundColor = "grey";
+        s.width = 100 + "px";
+        s.height = 200 + "px";
+        s.right = 50 + "px";
+        s.top = 20 + "px";
+    }
+    DiscardDeck();
+    Stapel();
 })(Uno2 || (Uno2 = {}));
 //# sourceMappingURL=Aufgabe2.js.map
--- Aufgabe2/Aufgabe2.ts
+++ Aufgabe2/Aufgabe2.ts
@@ -1,3 +1,14 @@
+/*
+Aufgabe: Aufgabe 2
+Name: Alea Flesch
+Matrikel: 259109
+Datum: 26.10.2018
+Hiermit versichere ich, dass ich diesen
+Code selbst in Zusammenarbeit mit Theresa Gut &
+mit Unterstützung von Lukas und Anna geschrieben habe. Er wurde
+nicht kopiert und auch nicht diktiert.
+*/
+
 namespace Uno2 {
 
     document.addEventListener("DOMContentLoaded", main);
@@ -24,8 +35,8 @@ namespace Uno2 {
     }
 
     //promt function
+    let x: number;
     function main(): void {
-        let x: number;
         let y: string = prompt("Wie viele Karten möchtest du?");
         x = Number(y);
 
@@ -37,14 +48,82 @@ namespace Uno2 {
 
     //random    
     function random() {
-        let r: number = Math.floor(Math.random() * (deck.length - 1));
-        
-        //Karte von deck in handcard kopiert       
-        handcards.push(deck[r]);
-
-        //kopierte Karte wurde aus deck gelöscht       
-        deck.splice(r);
+        for (let k: number = 0; k < x; k++) {
+            let r: number = Math.floor(Math.random() * (deck.length - 1));
+            handcards.push(deck[r]);                                      //Karte von deck in handcard kopiert       
+            deck.splice(r);                                                  //kopierte Karte wurde aus deck gelöscht 
+        }
     }
 
 
+    // Inhalte für HTML
+
+    function placeDiv(_color: string, _v: string, _y: number): void {
+        let div: HTMLDivElement = document.createElement("div");
+        document.body.appendChild(div);
+
+        div.setAttribute("id", "a" + _y);
+        document.getElementById("a" + _y).innerHTML += _v;
+        let s: CSSStyleDeclaration = div.style;
+        s.border = "thin solid black";
+        s.textAlign = "center";
+        s.position = "absolute";
+        s.backgroundColor = _color;
+        s.width = 100 + "px";
+        s.height = 200 + "px";
+        s.left = (_y + 0.2) * 110 + "px";
+        s.bottom = 40 + "px";
+        if (_color == "black") {
+            s.color = "white";
+        }
+        if (_color == "blue") {
+            s.color = "white";
+        }  //Schrift bei schwarzen und blauen Karten in weiß geändert
+
+
+    }
+
+    function Stapel() {
+        let div = document.createElement("div");
+        document.body.appendChild(div);
+        div.setAttribute("id", "CardDeck");
+        document.getElementById("CardDeck").innerHTML += "CardDeck";
+        let s = div.style;
+        s.border = "solid black";
+        s.textAlign = "center";
+        s.position = "absolute";
+        s.backgroundColor = "lightgray";
+        s.width = 100 + "px";
+        s.height = 200 + "px";
+        s.left = 50 + "px";
+        s.top = 20 + "px";
+    }
+
+    function DiscardDeck() {
+        let div = document.createElement("div");
+        document.body.appendChild(div);
+        div.setAttribute("id", "DiscardDeck");
+        document.getElementById("DiscardDeck").innerHTML += "DiscardDeck";
+        let s = div.style;
+        s.border = "solid black";
+        s.textAlign = "center";
+        s.position = "absolute";
+        s.backgroundColor = "grey";
+        s.width = 100 + "px";
+        s.height = 200 + "px";
+        s.right = 50 + "px";
+        s.top = 20 + "px";
+    }
+
+    DiscardDeck();
+    Stapel();
 }
+
+
+
+
+
+
+
+
+
